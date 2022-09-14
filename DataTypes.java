556c39fc498909ba7456e3736330d2ba68d38794
AydinT222 <70951773+AydinT222@users.noreply.github.com>
2022-09-14 12:50:37 -0400

    example

--- DataTypes.java
+++ DataTypes.java
@@ -7,9 +7,17 @@
  */
 public class DataTypes {
 
-    // Variable syntax: dataType varName = value;
-    int uriahFavInt = 5;
-    double snicker = 7.5;
-    boolean likeDogs = true;
+    public static void main(String args[]) {
+        // Variable syntax: dataType varName = value;
+        int uriahFavInt = 5;
+        double snicker = 7.5;
+        boolean likeDogs = true;
 
+        double n1 = 0.1;
+        double n2 = 0.2;
+        System.out.println("Uriah's Favorite Integer is: " + uriahFavInt + " and the class snicker: " + snicker);
+        System.out.println("floating point precision: " + (n1 + n2)); // floating point precision: 0.30000000000000004
+
+
+    }
 }
